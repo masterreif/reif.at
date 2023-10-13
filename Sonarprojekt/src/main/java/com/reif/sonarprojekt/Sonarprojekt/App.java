6e9bfcbaee6b818370cd4e2a7ffbf32dfd2a77f0
Gerald Reif <gerald-reif@gmx.at>
2023-10-13 16:38:13 +0200

    erster commit

---
+++ Sonarprojekt/src/main/java/com/reif/sonarprojekt/Sonarprojekt/App.java
@@ -0,0 +1,13 @@
+package com.reif.sonarprojekt.Sonarprojekt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
